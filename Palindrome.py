a4f4a46c80789cb06b35b6e0ef01a0cc364abc46
Manoj Kumar ponnana <84012313+Manoj-kumar-ponnana@users.noreply.github.com>
2022-10-29 10:02:29 +0530

    def rs(n):     rev=0     while n>0:         r=n%10         rev=rev*10+r         n=n//10     return rev a=int(input()) if a==rs(a):     print("True") else:     print("False")

--- Palindrome.py
+++ Palindrome.py
@@ -1,8 +1,12 @@
-n=int(input())
-t=n
-rev=0
-while n>0:
-    r=n%10
-    rev=rev*10+r
-    n=n//10
-print(t==rev)
+def rs(n):
+    rev=0
+    while n>0:
+        r=n%10
+        rev=rev*10+r
+        n=n//10
+    return rev
+a=int(input())
+if a==rs(a):
+    print("True")
+else:
+    print("False")
