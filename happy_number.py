72c2a9d05699c0f69062570cd6d225850f40eb52
Manoj Kumar ponnana <84012313+Manoj-kumar-ponnana@users.noreply.github.com>
2022-10-29 11:36:38 +0530

    def add_digit(n):     s=0     while n>0:         r=n%10         s+=r*r         n=n//10     if s//10 ==0:         return s     else:         return add_digit(s) a=int(input()) if add_digit(a) ==1 or add_digit(a)==7:     print("True") else:     print("False")

--- happy_number.py
+++ happy_number.py
@@ -1,13 +1,15 @@
-n=int(input())
-s=0
-while n>0:
-    r=n%10
-    s+=r**2
-    n//=10
-    if n==0 and s>9:
-        n=s
-        s=0
-if s==1 or s==7 :
-    print('True')
+def add_digit(n):
+    s=0
+    while n>0:
+        r=n%10
+        s+=r*r
+        n=n//10
+    if s//10 ==0:
+        return s
+    else:
+        return add_digit(s)
+a=int(input())
+if add_digit(a) ==1 or add_digit(a)==7:
+    print("True")
 else:
-    print('False')
+    print("False")
