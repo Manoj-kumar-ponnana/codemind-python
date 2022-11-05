4e1975ef0eb12b6a4aa206633561dbfd54484d02
Manoj Kumar ponnana <84012313+Manoj-kumar-ponnana@users.noreply.github.com>
2022-11-05 11:49:14 +0530

    def spf(n):     s=0     for i in range(1,n):         if n%i==0:             s+=i     return s n=int(input()) if spf(n)>n:     print("True") else:     print("False")

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,6 +1,11 @@
+def spf(n):
+    s=0
+    for i in range(1,n):
+        if n%i==0:
+            s+=i
+    return s
 n=int(input())
-s=0
-for i in range(1,n):
-    if n%i==0:
-        s+=i
-print("True") if s>n else print("False")
+if spf(n)>n:
+    print("True")
+else:
+    print("False")
